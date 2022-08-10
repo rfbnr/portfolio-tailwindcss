19d5d5f3423628990ee7e06f795d479b6fb6a1fc
ridwanfbnr <ridwanfebnur9@gmail.com>
2022-08-10 20:06:59 +0700

    hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.[html,js]"],
+  content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "16px"
+    },
+    extend: {
+      colors: {
+        primary: "#06b6d4",
+        secondary: "#64748b",
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320px"
+      }
+    },
   },
   plugins: [],
 }
