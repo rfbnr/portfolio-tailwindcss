4b51be7729965ac52877b859c152183cc0f39fea
ridwanfbnr <ridwanfebnur9@gmail.com>
2022-08-17 18:17:20 +0700

    added dark mode feature

--- public/js/script.js
+++ public/js/script.js
@@ -31,3 +31,27 @@ window.addEventListener("click", (e) => {
     navMenu.classList.add("hidden");
   }
 });
+
+// DarkMode Toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", () => {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// Move the toggle position according to the mode
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
